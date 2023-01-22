b0a16b9becac1f7b548c4ba19017aa1c9541383c
alexkayabula <alexkayabula@gmail.com>
2023-01-22 22:44:05 +0300

    use environment variables

--- lambda/google_analytics_to_s3.py
+++ lambda/google_analytics_to_s3.py
@@ -8,13 +8,14 @@ import boto3
 from oauth2client.service_account import ServiceAccountCredentials
 import requests
 
+S3_BUCKET = os.getenv('S3_BUCKET')
+SCOPE = os.getenv('SCOPE')
+GOOGLE_ANALYTICS_URL = os.getenv('GOOGLE_ANALYTICS_URL')
+s3_client = boto3.client('s3')
 
 def handler(event, context):
     try:
         data = []
-        s3_client = boto3.client('s3')
-        bucket = os.getenv('S3_BUCKET')
-
         # Fetch data from Google Analytics.
         access_token = generate_access_token()
         most_visited_pages = fetch_visited_pages(access_token=access_token)
@@ -32,7 +33,7 @@ def handler(event, context):
             values = list(item.values())
             writer.writerow(values)
             # Upload csv file to s3 bucket.
-            s3_client.put_object(Bucket=bucket, ContentType='text/csv', Key=fileName, Body=csvio.getvalue())
+            s3_client.put_object(Bucket=S3_BUCKET, ContentType='text/csv', Key=fileName, Body=csvio.getvalue())
     except Exception as e:
         logging.error(e)
 
@@ -48,10 +49,8 @@ def generate_access_token():
     Returns:
         An access token that is associated with the service account credentials.
     """
-    SCOPE = os.getenv('SCOPE')
-    s3_client = boto3.client('s3')
-    bucket = os.getenv('S3_BUCKET')
-    s3_json_object = s3_client.get_object(Bucket=bucket, Key="google_analytics_secrets.json")
+
+    s3_json_object = s3_client.get_object(Bucket=S3_BUCKET, Key="google_analytics_secrets.json")
     json_file = s3_json_object['Body'].read().decode("utf-8")
     json_data = json.loads(json_file)
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(keyfile_dict=json_data, scopes=[SCOPE])
@@ -61,7 +60,6 @@ def generate_access_token():
 
 # Fetch Most visited pages in the last 7 days data.
 def fetch_visited_pages(access_token):
-    GOOGLE_ANALYTICS_URL = os.getenv('GOOGLE_ANALYTICS_URL')
     url = f'{GOOGLE_ANALYTICS_URL}{access_token}'
     try:
         logging.info("[Google Analytics] Fetching user mos visited pages data.")
