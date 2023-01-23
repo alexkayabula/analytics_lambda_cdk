5d27b2d0eb367d5c9ac46f8f2e3528024f8a435d
alexkayabula <alexkayabula@gmail.com>
2023-01-23 23:11:01 +0300

    load csv data to AWS RDS PostgresSQL

--- lambda/s3_to_postgresql.py
+++ lambda/s3_to_postgresql.py
@@ -1,4 +1,38 @@
-import json
+import os
+import sys
+import logging
+import psycopg2
 
-def handler (event, context):
-    return "I have been invoked."
+
+#AWS RDS environment settings
+rds_host  = os.getenv("RDS_POSTGRES_HOST")
+db_name = os.getenv("RDS_POSTGRES_DB_NAME")
+name = os.getenv("RDS_POSTGRES_DB_USER")
+password = os.getenv("RDS_POSTGRES_DB_PASSWORD")
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = psycopg2.connect(host=rds_host, user=name, password=password, database=db_name)
+except psycopg2.DatabaseError as e:
+    logger.error("ERROR: Unexpected error: Could not connect to PostgreSQL instance.")
+    logger.error(e)
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS PostgreSQL instance succeeded.")
+
+def handler(event, context):
+    """
+    Fetch content from s3 to RDS PostgreSQL instance database.
+    """
+
+    with conn.cursor() as cur:
+        cur.execute("CREATE EXTENSION aws_s3 CASCADE;")
+        cur.execute("DROP TABLE IF EXISTS most_visited_pages_7days;")
+        cur.execute("CREATE TABLE most_visited_pages_7days(page varchar(255), views varchar(255), users varchar(255), viewsPerUser varchar(255), avgEngagementTime varchar(255));")
+        cur.execute("SELECT aws_s3.table_import_from_s3( 'most_visited_pages_7days','','(format csv)','(ga-bucket,test-23-01-2023.csv,ap-northeast-1)');")
+    conn.commit()
+
+    logger.info("SUCCESS: Added items to RDS PostgreSQL table.")
+    return "SUCCESS: Added items to RDS PostgreSQL table." 
