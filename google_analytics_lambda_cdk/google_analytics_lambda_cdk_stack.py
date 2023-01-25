8cae921f03b8cf47d5fe6a8c267f797bab72fa87
alexkayabula <alexkayabula@gmail.com>
2023-01-25 11:03:14 +0300

    update lambda excetution time

--- google_analytics_lambda_cdk/google_analytics_lambda_cdk_stack.py
+++ google_analytics_lambda_cdk/google_analytics_lambda_cdk_stack.py
@@ -36,7 +36,7 @@ class GoogleAnalyticsLambdaCdkStack(Stack):
         rule_one = events.Rule(
             self,  "RuleOne",
             schedule=events.Schedule.cron(
-                minute='5',
+                minute='20',
                 hour='8',
                 month='*',
                 week_day='MON-SUN',
@@ -47,7 +47,7 @@ class GoogleAnalyticsLambdaCdkStack(Stack):
         rule_two = events.Rule(
             self, "RuleTwo",
             schedule=events.Schedule.cron(
-                minute='10',
+                minute='25',
                 hour='8',
                 month='*',
                 week_day='MON-SUN',
