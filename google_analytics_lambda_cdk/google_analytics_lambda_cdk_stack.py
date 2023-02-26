8d990d80aeea688c3bf1d5438b3f8e297d46d599
kayz <alexkayabula@gmail.com>
2023-02-26 17:30:02 +0300

    configure secret

--- google_analytics_lambda_cdk/google_analytics_lambda_cdk_stack.py
+++ google_analytics_lambda_cdk/google_analytics_lambda_cdk_stack.py
@@ -49,7 +49,10 @@ class GoogleAnalyticsLambdaCdkStack(Stack):
         s3_bucket.grant_read_write(rds_import_role)
 
         # Simple secret
-        secret = secretsmanager.Secret(self, "secret")
+        secret = secretsmanager.Secret(
+            self, "Secret",
+            secret_name="secret"
+            )
 
         # Create an RDS instance
         vpc = ec2.Vpc(self, "Vpc")
