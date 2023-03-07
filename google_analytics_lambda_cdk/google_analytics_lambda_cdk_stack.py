cbacbdb9835b415dc5e3e0bed13da6faf6b4dc9a
kayz <alexkayabula@gmail.com>
2023-03-07 13:00:50 +0300

    Update VPC settings

--- google_analytics_lambda_cdk/google_analytics_lambda_cdk_stack.py
+++ google_analytics_lambda_cdk/google_analytics_lambda_cdk_stack.py
@@ -86,18 +86,7 @@ class GoogleAnalyticsLambdaCdkStack(Stack):
 
 
         # Create a VPC
-        vpc = ec2.Vpc(
-            self, 'AnalyticsVpc',
-            cidr='10.0.0.0/16',
-            max_azs=2,
-            subnet_configuration=[
-                ec2.SubnetConfiguration(
-                    name='public',
-                    subnet_type=ec2.SubnetType.PUBLIC,
-                    cidr_mask=24
-                )
-            ]
-        )
+        vpc = ec2.Vpc(self, "Vpc")
 
         # Cost optimization
         # Reference: https://aws.amazon.com/premiumsupport/knowledge-center/vpc-reduce-nat-gateway-transfer-costs/
