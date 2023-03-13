6006473d863647cc871f06fae3a8dae3d69f5629
kayz <alexkayabula@gmail.com>
2023-03-13 13:12:19 +0300

    restructure project

--- google_analytics/website/google_analytics_to_s3.py
+++ google_analytics/website/google_analytics_to_s3.py
@@ -5,7 +5,7 @@ import logging
 import csv
 from datetime import datetime
 import boto3
-from google_analytics.website.data.helper import generate_access_token
+from data.helper import generate_access_token
 from data.data_last_7days import fetch_visited_pages
 from data.data_last_365days import fetch_visited_pages_365days
 
