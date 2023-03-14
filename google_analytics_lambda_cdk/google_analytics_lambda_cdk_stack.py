89296e7a65abedf3dffdfd982a8d237a3a69edcf
kayz <alexkayabula@gmail.com>
2023-03-14 15:56:28 +0300

    Add layer

--- google_analytics_lambda_cdk/google_analytics_lambda_cdk_stack.py
+++ google_analytics_lambda_cdk/google_analytics_lambda_cdk_stack.py
@@ -141,7 +141,7 @@ class GoogleAnalyticsLambdaCdkStack(Stack):
         google_analytics_to_s3_lambda = _lambda.Function(
             self, 'GoogleAnalyticsToS3Handler',
             runtime=_lambda.Runtime.PYTHON_3_8,
-            code=_lambda.Code.from_asset('.build/lambdas/website'),
+            code=_lambda.Code.from_asset('.build/lambdas/google_analytics/website'),
             handler='google_analytics_to_s3.handler',
             timeout=Duration.seconds(180),
             role=lambda_role,
@@ -151,7 +151,7 @@ class GoogleAnalyticsLambdaCdkStack(Stack):
         s3_to_postgresql_lambda = _lambda.Function(
             self, 'S3ToPostgresHandler',
             runtime=_lambda.Runtime.PYTHON_3_8,
-            code=_lambda.Code.from_asset('.build/lambdas/webiste'),
+            code=_lambda.Code.from_asset('.build/lambdas/google_analytics/website'),
             handler='s3_to_postgresql.handler',
             timeout=Duration.seconds(180),
             role=lambda_role,
